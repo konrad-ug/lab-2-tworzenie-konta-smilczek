24b3d2b23dd1e9c893da07403389be2d4a9fa3c4
smilczek <s.milczek.670@studms.ug.edu.pl>
2022-10-20 17:29:36 +0200

    added a couple tests

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -8,11 +8,24 @@ class TestCreateBankAccount(unittest.TestCase):
         imie = "dariusz"
         nazwisko = "januszewski"
         pesel = "12345678910"
+        n_pesel = "1234"
+        kod_rabatowy = "PROM_XYZ"
+        n_kod_rabatowy = "PROM_ZYX"
+
         pierwsze_konto = Konto(imie, nazwisko, pesel)
-        self.assertEqual(pierwsze_konto.pesel, pesel, "Pesel nie został zapisany!")
-        self.assertEqual(len(pierwsze_konto.pesel), 11, "Pesel nie posiada odpowiedniej liczby znaków!")
         self.assertEqual(pierwsze_konto.imie, imie, "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, nazwisko, "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        
 
     #tutaj proszę dodawać nowe testy
+        self.assertEqual(pierwsze_konto.pesel, pesel, "Pesel nie został zapisany!")
+        self.assertEqual(len(pierwsze_konto.pesel), 11, "Pesel nie posiada odpowiedniej liczby znaków!")
+        
+        drugie_konto = Konto(imie, nazwisko, n_pesel, kod_rabatowy)
+        self.assertEqual(drugie_konto.saldo, 50, "Kod rabatowy został niepoprawnie sprawdzony!")
+        self.assertEqual(drugie_konto.pesel, "Niepoprawny pesel!", "Pesel nie został poprawnie sprawdzony w konstruktorze")
+
+        trzecie_konto = Konto(imie, nazwisko, pesel, n_kod_rabatowy)
+        self.assertEqual(trzecie_konto.saldo, 0, "Kod rabatowy został niepoprawnie sprawdzony!")
+
