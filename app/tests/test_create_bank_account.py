6db47ebd54f91da0a8905a4aacf4c039acdc0d49
smilczek <s.milczek.670@studms.ug.edu.pl>
2022-10-20 17:02:27 +0200

    added field 'pesel' to Konto class, added test for pesel in test_create_bank_account.py

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -5,7 +5,8 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "12345678910")
+        self.assertEqual(pierwsze_konto.pesel, "12345678910", "Pesel nie został zapisany!")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
