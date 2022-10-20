b2dcf6a6fba02166ad43387587c93c37171142bc
smilczek <s.milczek.670@studms.ug.edu.pl>
2022-10-20 17:04:49 +0200

    added a test for the length of pesel

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -7,6 +7,7 @@ class TestCreateBankAccount(unittest.TestCase):
     def test_tworzenie_konta(self):
         pierwsze_konto = Konto("Dariusz", "Januszewski", "12345678910")
         self.assertEqual(pierwsze_konto.pesel, "12345678910", "Pesel nie został zapisany!")
+        self.assertEqual(len(pierwsze_konto.pesel), 11, "Pesel nie posiada odpowiedniej liczby znaków!")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
