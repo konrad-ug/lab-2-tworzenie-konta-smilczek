ee40ce7fd9b07e48a613c9cba5131c46b0be4a47
smilczek <s.milczek.670@studms.ug.edu.pl>
2022-10-20 17:32:55 +0200

    new functionality with discount code added in Konto class

--- app/Konto.py
+++ app/Konto.py
@@ -1,9 +1,17 @@
 class Konto:
-    def __init__(self, imie, nazwisko, pesel):
+    def __init__(self, imie, nazwisko, pesel, kod_rabatowy=None):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.saldo = 0
+
         if len(pesel) == 11:
             self.pesel = pesel
         else:
             self.pesel = "Niepoprawny pesel!"
+
+        if kod_rabatowy:
+            if kod_rabatowy == "PROM_XYZ"
+                self.saldo = 50
+            else:
+                self.saldo = 0
+        else:
+            self.saldo = 0
