ecd15715e3103150d6b362c5b1d26fe4f72e37bb
smilczek <s.milczek.670@studms.ug.edu.pl>
2022-10-22 14:14:14 +0200

    lab02 done

--- app/Konto.py
+++ app/Konto.py
@@ -2,15 +2,14 @@ class Konto:
     def __init__(self, imie, nazwisko, pesel, kod_rabatowy=None):
         self.imie = imie
         self.nazwisko = nazwisko
+
         if len(pesel) == 11:
             self.pesel = pesel
         else:
             self.pesel = "Niepoprawny pesel!"
 
+        self.saldo = 0
         if kod_rabatowy:
             if kod_rabatowy == "PROM_XYZ":
-                self.saldo = 50
-            else:
-                self.saldo = 0
-        else:
-            self.saldo = 0
+                if int(pesel[:2]) > 60:
+                    self.saldo += 50
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -9,18 +9,17 @@ old_pesel = "12345678910"
 n_pesel = "1234"
 kod_rabatowy = "PROM_XYZ"
 n_kod_rabatowy = "PROM_ZYX"
-class TestCreateBankAccount(unittest.TestCase):
 
+class TestCreateBankAccount(unittest.TestCase):
     def test_tworzenie_konta(self):
         pierwsze_konto = Konto(imie, nazwisko, pesel)
         self.assertEqual(pierwsze_konto.imie, imie, "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, nazwisko, "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
-        
 
     #tutaj proszę dodawać nowe testy
         self.assertEqual(pierwsze_konto.pesel, pesel, "Pesel nie został zapisany!")
-        self.assertEqual(len(pierwsze_konto.pesel), 11, "Pesel nie posiada odpowiedniej liczby znaków!")
+        self.assertEqual(len(pierwsze_konto.pesel), 11, "Pesel nie został zapisany!")
         
     def test_pesel(self):
         pierwsze_konto = Konto(imie, nazwisko, n_pesel)
@@ -29,6 +28,7 @@ class TestCreateBankAccount(unittest.TestCase):
     def test_kod_rabatowy(self):
         pierwsze_konto = Konto(imie, nazwisko, pesel, kod_rabatowy)
         self.assertEqual(pierwsze_konto.saldo, 50, "Kod rabatowy został niepoprawnie sprawdzony!")
+
         drugie_konto = Konto(imie, nazwisko, pesel, n_kod_rabatowy)
         self.assertEqual(drugie_konto.saldo, 0, "Kod rabatowy został niepoprawnie sprawdzony!")
 
