98335b835b76427ea9e939ebad8336879efc85ca
smilczek <s.milczek.670@studms.ug.edu.pl>
2022-10-22 13:26:34 +0200

    fixed errors in test_create_bank_account.py

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -2,35 +2,33 @@ import unittest
 
 from ..Konto import Konto
 
+imie = "dariusz"
+nazwisko = "januszewski"
+pesel = "12345678910"
+n_pesel = "1234"
+rok_ur = "1960"
+kod_rabatowy = "PROM_XYZ"
+n_kod_rabatowy = "PROM_ZYX"
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
-        imie = "dariusz"
-        nazwisko = "januszewski"
-        pesel = "12345678910"
-        n_pesel = "1234"
-        rok_ur = "1960"
-        kod_rabatowy = "PROM_XYZ"
-        n_kod_rabatowy = "PROM_ZYX"
-
         pierwsze_konto = Konto(imie, nazwisko, pesel, rok_ur)
         self.assertEqual(pierwsze_konto.imie, imie, "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, nazwisko, "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
-        self.assertEqual(pierwsze_konto.rok_ur, "1960", "Rok urodzenia nie został zapisany!")
         
 
     #tutaj proszę dodawać nowe testy
+        self.assertEqual(pierwsze_konto.rok_ur, "1960", "Rok urodzenia nie został zapisany!")
         self.assertEqual(pierwsze_konto.pesel, pesel, "Pesel nie został zapisany!")
         self.assertEqual(len(pierwsze_konto.pesel), 11, "Pesel nie posiada odpowiedniej liczby znaków!")
         
-    def test_pesel:
-        pierwsze_konto = Konto(imie, nazwisko, n_pesel)
+    def test_pesel(self):
+        pierwsze_konto = Konto(imie, nazwisko, n_pesel, rok_ur)
         self.assertEqual(pierwsze_konto.pesel, "Niepoprawny pesel!", "Pesel nie został poprawnie sprawdzony w konstruktorze")
 
-    def test_kod_rabatowy
-        pierwsze_konto = Konto(imie, nazwisko, pesel, rok_ur, kod_rabatowy)
-        drugie_konto = Konto(imie, nazwisko, n_pesel, rok_ur, kod_rabatowy)
+    def test_kod_rabatowy(self):
+        drugie_konto = Konto(imie, nazwisko, pesel, rok_ur, kod_rabatowy)
         self.assertEqual(drugie_konto.saldo, 50, "Kod rabatowy został niepoprawnie sprawdzony!")
 
         trzecie_konto = Konto(imie, nazwisko, pesel, n_kod_rabatowy)
