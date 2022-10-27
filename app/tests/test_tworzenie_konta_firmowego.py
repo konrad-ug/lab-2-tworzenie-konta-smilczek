dff0cd1574c4ada36217fdbe9969d26bb4ce4d37
smilczek <s.milczek.670@studms.ug.edu.pl>
2022-10-27 16:56:18 +0200

    created files for KontoFirmowe and testing it, wrote first tests for creating objects of KontoFirmowe class

---
+++ app/tests/test_tworzenie_konta_firmowego.py
@@ -0,0 +1,13 @@
+import unittest
+
+from ..KontoFirmowe import KontoFirmowe
+
+
+nazwa_firmy = "Paruwex sp. z o.o"
+nip = "1234567890"
+class TestTworzenieKontaFirmowego(unittest.TestCase):
+    def test_tworzenie_konta(self):
+        pierwsze_konto = KontoFirmowe(nazwa_firmy, nip)
+        self.assertEqual(pierwsze_konto.nazwa_firmy, nazwa_firmy, "Nazwa firmy nie została zapisana!")
+        self.assertEqual(pierwsze_konto.nip, nip, "Nr NIP nie został zapisany!")
+        self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
