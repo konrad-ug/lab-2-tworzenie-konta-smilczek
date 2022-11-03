dffdfb5ba3865641a9d9e08bc59807d03801b6d4
smilczek <s.milczek.670@studms.ug.edu.pl>
2022-11-03 17:43:28 +0100

    broke the code, shouldn't get a pass now

--- app/Konto.py
+++ app/Konto.py
@@ -28,3 +28,6 @@ class Konto:
     def zaksieguj_przelew_ekspresowy(self, kwota):
         if self.saldo >= kwota:
             self.saldo -= kwota + self.oplata_za_ekspres
+
+    def empty(self):
+        pass
