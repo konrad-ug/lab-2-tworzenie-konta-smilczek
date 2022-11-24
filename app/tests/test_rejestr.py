8554f6957145924e5d66a35c274223429e5ff137
smilczek <s.milczek.670@studms.ug.edu.pl>
2022-11-24 18:33:02 +0100

    created setUpClass and tearDownClass, tests still pass

--- app/tests/test_rejestr.py
+++ app/tests/test_rejestr.py
@@ -13,20 +13,23 @@ nazwisko2 = "Kowalski"
 pesel2 = "69345612377"
 
 class TestRejestr(unittest.TestCase):
-    # def setUpClass():
-    #     RejestrKont.dodaj_konto(Konto(imie, nazwisko, pesel))
+    def setUpClass():
+        RejestrKont.dodaj_konto(Konto(imie, nazwisko, pesel))
     
-    def test_1dodawanie_pierwszego_konta(self):
-        konto = Konto(imie, nazwisko, pesel)
-        RejestrKont.dodaj_konto(konto)
-        self.assertEqual(RejestrKont.ile_kont(), 1)
+    def tearDownClass():
+        RejestrKont.konta = []
+
+    # def test_1_dodawanie_pierwszego_konta(self):
+    #     konto = Konto(imie, nazwisko, pesel)
+    #     RejestrKont.dodaj_konto(konto)
+    #     self.assertEqual(RejestrKont.ile_kont(), 1)
         
-    def test_2dodawanie_drugiego_konta(self):
+    def test_2_dodawanie_drugiego_konta(self):
         konto = Konto(imie, nazwisko, pesel)
         RejestrKont.dodaj_konto(konto)
         self.assertEqual(RejestrKont.ile_kont(), 2)
         
-    def test_szukanie_konta(self):
+    def test_3_szukanie_konta(self):
         konto = Konto(imie2, nazwisko2, pesel2)
         RejestrKont.dodaj_konto(konto)
         self.assertEqual(RejestrKont.wyszukaj_konto_z_peselem(pesel2), konto)
