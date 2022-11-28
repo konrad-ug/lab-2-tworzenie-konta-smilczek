dbfa585d2a7e04900abde1439674752f0e203e22
smilczek <s.milczek.670@studms.ug.edu.pl>
2022-11-28 16:29:23 +0100

    added '/' in each request at the beggining in api.py

--- app/api.py
+++ app/api.py
@@ -4,7 +4,7 @@ from app.Konto import Konto
 
 app = Flask(__name__)
 
-@app.route("konta/stworz_konto", methods=['POST'])
+@app.route("/konta/stworz_konto", methods=['POST'])
 def stworz_konto():
     dane = request.get_json()
     print(f"Request o stworzenie konta z danymi: {dane}")
@@ -12,13 +12,13 @@ def stworz_konto():
     RejestrKont.dodaj_konto(konto)
     return jsonify("Konto stworzone"), 201
 
-@app.route("konta/ile_kont", methods=['GET'])
+@app.route("/konta/ile_kont", methods=['GET'])
 def ile_kont():
     print("Request o podanie liczby kont")
     liczba_kont = RejestrKont.ile_kont()
     return jsonify(liczba_kont), 200
 
-@app.route("konta/konto/<pesel>", methods=['GET'])
+@app.route("/konta/konto/<pesel>", methods=['GET'])
 def wyszukaj_konto_z_peselem(pesel):
     print("Request o wyszukanie konta po numerze pesel")
     konto = RejestrKont.wyszukaj_konto_z_peselem(pesel)
