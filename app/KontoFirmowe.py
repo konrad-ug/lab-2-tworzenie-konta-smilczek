4f5466ca462bd89f49f9a0b74ef09168211e56d2
smilczek <s.milczek.670@studms.ug.edu.pl>
2022-12-21 23:56:56 +0100

    don't really understand the task, made zapytaj_o_nip() return response.status_code == 200

--- app/KontoFirmowe.py
+++ app/KontoFirmowe.py
@@ -1,16 +1,25 @@
 from app.Konto import Konto
+from datetime import date
+import os
+import requests
 
 
 class KontoFirmowe(Konto):
     def __init__(self, nazwa_firmy, nip):
+        self.nip = nip if self.zapytaj_o_nip(nip) else "Pranie!"
         self.nazwa_firmy = nazwa_firmy
-        self.nip = nip if self.czy_poprawny_nip(nip) else "Niepoprawny NIP!"
         self.saldo = 0
         self.oplata_za_ekspres = 5
         self.historia = []
 
     def czy_poprawny_nip(self, nip):
         return len(nip) == 10
+    
+    def zapytaj_o_nip(self, nip):
+        url = os.environ('BANK_APP_MF_URL')
+        date = date.today().strftime("%Y-%m-%d")
+        response = requests.get(url + nip, json={"date": date})
+        return response.status_code == 200
 
     def kredyt_zasluzony(self, kwota):
         if -1755 in self.historia:
